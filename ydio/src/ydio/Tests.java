b638a21e96883bf014542fbcae5cec321ba2ac17
Mateusz Czernecki <mathck@gmail.com>
2013-12-06 14:25:38 +0100

    test class

---
+++ ydio/src/ydio/Tests.java
@@ -0,0 +1,24 @@
+package ydio;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class Tests {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
