279686c279f0bd0ad3b69339dc40f94c8a176caf
Benjamin Ilg <benjamin.ilg@gmx.de>
2014-01-19 04:34:29 +0100

    data_type neudefinition mit drei Varianten der Forscherdaten

--- ydio/src/ydio/data_type.java
+++ ydio/src/ydio/data_type.java
@@ -6,11 +6,9 @@ package ydio;
  * @created 04-Dez-2013 20:51:13
  */
 public enum data_type {
-	ydiotCount,
-	likeDislikeReportStats,
-	avgContentLength,
-	maleVsFemale,
-	avgAge,
-	avgPasswordLength,
-	wordCount_FuckStupidIdiotGayNoob
+	BEITRAG_STATS, // gibt alle Beiträge mit erstellungsdatum und Statistiken an
+	YDIOT_STATS, // gibt alle Ydioten mit Anzahl von Beiträgen an
+	GLOBAL_STATS; // gibt Ydioten mit Beitrag Stats an, Achtung! Sehr umfangreich!
+	
+	
 }
